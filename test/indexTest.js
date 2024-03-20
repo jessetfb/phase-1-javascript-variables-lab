99537e2be28fea78e12b7237581452ce585b383d
jesse.langat@student.moringaschool.com <jesse.langat@student.moringaschool.com>
2024-03-20 10:43:27 +0300

    phase-1-javascript-variables-lab

--- test/indexTest.js
+++ test/indexTest.js
@@ -5,8 +5,8 @@ const path = require('path')
 
 const js = fs.readFileSync(path.resolve(__dirname, '..', 'index.js'), 'utf-8')
 
-describe('index.js', function () {
-  describe('companyName', function () {
+describe (`index.js`, function(){
+  describe(`companyName` ,function (){
     it('is set as Scuber', function () {
       expect(companyName).to.equal('Scuber');
     });
